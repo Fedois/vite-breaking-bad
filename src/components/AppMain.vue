b6977d4ee52abe2714f98ee87b45be65039c69c8
Fedois <fedefox14@gmail.com>
2023-02-06 15:45:58 +0100

    aggiunta card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,9 +6,76 @@ export default{
 </script>
 
 <template>
-main
+    <div class="container p-5 bg-white">
+        <section class=" my-found bg-dark text-white w-100">
+            <p class="p-3 m-0">found 39 cards</p>
+        </section>
+
+        <section class="my-cards row text-center">
+            <div class="col-2">
+                <div class="img-card pb-2">
+                    <img src="" alt="">
+                </div>
+                <div class="info-card">
+                    <h3 class="text-white">ciao</h3>
+                    <p>ciao</p>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="img-card pb-2">
+                    <img src="" alt="">
+                </div>
+                <div class="info-card">
+                    <h3 class="text-white">ciao</h3>
+                    <p>ciao</p>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="img-card pb-2">
+                    <img src="" alt="">
+                </div>
+                <div class="info-card">
+                    <h3 class="text-white">ciao</h3>
+                    <p>ciao</p>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="img-card pb-2">
+                    <img src="" alt="">
+                </div>
+                <div class="info-card">
+                    <h3 class="text-white">ciao</h3>
+                    <p>ciao</p>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="img-card pb-2">
+                    <img src="" alt="">
+                </div>
+                <div class="info-card">
+                    <h3 class="text-white">ciao</h3>
+                    <p>ciao</p>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="img-card pb-2">
+                    <img src="" alt="">
+                </div>
+                <div class="info-card">
+                    <h3 class="text-white">ciao</h3>
+                    <p>ciao</p>
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
 
 <style scoped>
-
+.img-card{
+    height: 200px;
+    background-color: red;
+}
+.info-card{
+    background-color: orange;
+}
 </style>
