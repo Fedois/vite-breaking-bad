1893033fb35afef7da65e4fea71a0abeb751de34
Fedois <fedefox14@gmail.com>
2023-02-06 18:07:05 +0100

    aggiunta logo yu-gi-oh

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,10 +10,14 @@ export default {
 
 <template>
     <div class="title py-2 px-4 bg-white">
-        <h1>Yu-Gi-Oh Api</h1>
+        <img class="align-middle" src="../assets/172-1722617_yu-gi-oh-cards-logo-hd-png-download.png" alt="">
+        <h1 class="align-middle d-inline-block ms-3">Yu-Gi-Oh Api</h1>
     </div>
     <SearchInput />
 </template>
 
 <style lang="scss" scoped>
+.title > img{
+    width: 80px;
+}
 </style>
